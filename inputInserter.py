c5ec8b1be360fa3f5dde2c09bd2e13b4dca30ea9
Stuart Simon <ssimo3@lsuhsc.edu>
2022-06-08 12:26:30 -0500

    Changed Python to work on new HTML file

--- inputInserter.py
+++ inputInserter.py
@@ -1,84 +1,44 @@
-import locale
+import locale, re
 
 from bs4 import BeautifulSoup, Tag
 from bs4.element import NavigableString
 
+current_tag_match = re.Match()
 
-class HouseholdIncomeLimit:
-    def __init__(self, household_size: int, max_income: int, article: str = "a"):
-        self.household_size = household_size
-        self.max_income = max_income
-        self.article = article
 
 def main():
     locale.setlocale(locale.LC_ALL, "en-US")
-    with open("jotFormCustomWidget.html", "r+", encoding="utf-8") as customWidget:
-        customWidgetSoup = BeautifulSoup(customWidget, "html.parser")
-        inputTag = customWidgetSoup.find("input")
+    with open("ehs_scholarship_application.html", "r+", encoding="utf-8") as scholarship_application:
+        scholarship_application_soup = BeautifulSoup(scholarship_application, "html.parser")
+        inputTag = scholarship_application_soup.find(match_input)
         while inputTag is not None:
-            labelTag = customWidgetSoup.find("label", attrs={
+            label_tag = inputTag.find_next_sibling("label", attrs={
                 "for": inputTag["id"]
-                })
-            if labelTag is not None:
-                labelTag.decompose()
-            inputTag.decompose()
-            inputTag = customWidgetSoup.find("input")
-        customWidgetSoup.smooth()
-        fieldset = customWidgetSoup.find("fieldset")
-        if type(fieldset.contents[-1]) == NavigableString:
-            fieldset.contents[-1].extract()
-        fieldset.append("\r\n    ")
-        input_index = 1
-        for householdIncomeLimit in [
-            HouseholdIncomeLimit(2, 3939),
-            HouseholdIncomeLimit(3, 4866),
-            HouseholdIncomeLimit(4, 5793),
-            HouseholdIncomeLimit(5, 6720),
-            HouseholdIncomeLimit(6, 7646),
-            HouseholdIncomeLimit(7, 7820),
-            HouseholdIncomeLimit(8, 7994, "an"),
-            HouseholdIncomeLimit(9, 8168)
-        ]:
-            div = customWidgetSoup.new_tag("div");
-
-            div.append(customWidgetSoup.new_tag("input", attrs={
-                "name": "household_income",
-                "id": "household_income_" + str(input_index),
-                "type": "radio",
-                "value": "For " + householdIncomeLimit.article + " " + str(householdIncomeLimit.household_size) + "-person household, my monthly income is $" + locale.format_string("%d", householdIncomeLimit.max_income, grouping=True, monetary=True) + " or less."
-            }))
-            div.append(" ")
-            label = customWidgetSoup.new_tag("label", attrs={
-                "for": "household_income_" + str(input_index)
             })
-            label.append("For " + householdIncomeLimit.article + " ")
-            first_strong = customWidgetSoup.new_tag("strong")
-            first_strong.append(str(householdIncomeLimit.household_size) + "-person household,")
-            label.append(first_strong)
-            label.append(" my monthly income is ")
-            second_strong = customWidgetSoup.new_tag("strong")
-            second_strong.append(locale.format_string("$%d or less.", householdIncomeLimit.max_income, grouping=True, monetary=True))
-            label.append(second_strong)
-            div.append(label)
-            fieldset.append(div)
-            fieldset.append("\r\n    ")
-            input_index += 1
-        fieldset.append(customWidgetSoup.new_tag("input", attrs={
-            "name": "household_income",
-            "id": "household_income_" + str(input_index),
-            "type": "radio",
-            "value": "My monthly income exceeds these options based on my household."
-        }))
-        fieldset.append(" ")
-        label = customWidgetSoup.new_tag("label", attrs={
-            "for": "household_income_" + str(input_index)
-        })
-        label.append("My monthly income exceeds these options based on my household.")
-        fieldset.append(label)
-        customWidget.seek(0, 0)
-        customWidget.truncate(0)
-        customWidget.seek(0, 0)
-        customWidget.write(str(customWidgetSoup))
+            label_tag.clear(True)
+            first_strong_tag = scholarship_application_soup.new_tag("strong")
+            first_strong_tag.append(current_tag_match.group("first_strong"))
+            second_strong_tag = scholarship_application_soup.new_tag("strong")
+            second_strong_tag.append(current_tag_match.group("second_strong"))
+            label_tag.extend([
+                "For " + current_tag_match.group("article") + " ",
+                first_strong_tag,
+                " my monthly income is ",
+                second_strong_tag
+            ])
+
+        scholarship_application.seek(0, 0)
+        scholarship_application.truncate(0)
+        scholarship_application.seek(0, 0)
+        scholarship_application.write(str(scholarship_application_soup))
+
+
+def match_input(tag: Tag):
+    if tag.name.lower() != "input" or tag["type"].lower() != "radio":
+        return False
+    current_tag_match = re.match(r"^For (?P<article>an?) (?P<first_strong>(?P<household_size>\d+)-person household,) my monthly income is (?P<second_strong>\$(?P<max_monthly_income>\d{1,3}(?:,\d{3})*) or less.)$", tag["value"])
+    return current_tag_match is not None
+
 
 if __name__ == "__main__":
     main()
